f76edd4a8aa58413b5836f787701d433a58a502b
ali <eng.aliali2011@gmail.com>
2018-03-01 16:55:24 +0200

    First commit

---
+++ src/main/java/com/skywriter/gateway/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.skywriter.gateway.repository.search;
+
+import com.skywriter.gateway.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
